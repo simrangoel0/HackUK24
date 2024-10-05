04c5e3a2100505fc04d12d00e4589a877e569a51
arkink1 <63444196+arkink1@users.noreply.github.com>
2024-10-05 23:17:29 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ def fact_check():
     data = request.get_json()
     print(data)
     check = fact_check_fn(data["claim"], data["num_articles"])
-    print(check)
-    return jsonify(check), 200
+    check2 = check[8:len(check)-4]
+    check_json = jsonify(check2)
+    print(check_json)
+    return check_json, 200
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,14 +1,18 @@
 import requests
 import json
 
-data = {"claim": "1 gram of fentanyl is lethal"}
+data = {"claim": "1 gram of fentanyl is lethal", "num_articles":1}
 r = requests.post("http://localhost:5000/", json=data)
 
 print(r.status_code)
 print(type(r.content))
 print(r.content)
 rr = r.json()
+# rr = rr[8:len(rr)-4]
 print(rr)
+# print(type(rr))
+
+print()
 
 
 print("done")
