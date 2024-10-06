5a1e23a61dd28a1d322d047e6da8291ceb117a11
arkink1 <63444196+arkink1@users.noreply.github.com>
2024-10-06 12:29:14 +0100

    Add files via upload
    img video support

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
-from fact_checker import fact_check_fn
+from fact_checker import fact_check_fn, fact_check_fn_img, fact_check_fn_video
+import os
 
 app = Flask(__name__)
 
@@ -7,9 +8,30 @@ app = Flask(__name__)
 def fact_check():
     data = request.get_json()
     print(data)
-    check = fact_check_fn(data["claim"], data["num_articles"])
-    check2 = check[8:len(check)-4]
-    check_json = jsonify(check2)
+    
+    if "file" in data:
+        file = data["file"]
+        file_path = os.path.join('/tmp', file.filename)
+        file.save(file_path)
+        print(f"File saved to {file_path}")
+
+        if file.mimetype.startswith('image/'):
+            print("The file is an image.")
+            check = fact_check_fn_img(file_path, data["num_articles"])
+        elif file.mimetype.startswith('video/'):
+            print("The file is a video.")
+            check = fact_check_fn_video(file_path, data["num_articles"])
+        else:
+            print("The file is neither an image nor a video.")
+            check = fact_check_fn(data["claim"], data["num_articles"])
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            print(f"Temporary file {file_path} has been deleted.")
+    else:
+        print("No file given.")
+        check = fact_check_fn(data["claim"], data["num_articles"])
+
+    check_json = jsonify(check)
     print(check_json)
     return check_json, 200
 
--- fact_checker.py
+++ fact_checker.py
@@ -2,6 +2,8 @@ import os
 from mistralai import Mistral
 import requests
 import json
+import base64
+from video import get_transcription
 
 mistral_api_key = os.environ["MISTRAL_API_KEY"]
 brave_api_key = os.environ["BRAVE_API_KEY"]
@@ -43,7 +45,7 @@ def fact_check_fn(claim, num_articles):
             examples[3], 
             {
                 "role": "user",
-                "content": "Claim:" + claim + ". Write a search query that will search the web to figure out whether the claim is true or not. Return just one search query that will be inputted to the search engine. Be succinct but thorough. Some examples are {claim: Kylian Mbappe is one of the highest paid sportsmen. query: Highest paid sportspeople in the world. claim: Trump was a better president than Biden. query: Who was the better president between Trump and Biden}"
+                "content": "Claim:" + claim + ". Write a search query that will search the web to figure out whether the claim is true or not. Return just one search query that will be inputted to the search engine. Be succinct but thorough."
             },
         ]
     )
@@ -78,18 +80,65 @@ def fact_check_fn(claim, num_articles):
         messages = [
             {
                 "role": "system",
-                "content": "You are a fact checker LLM. Verify the claim with reference to each of the sources, rating them on how much they agree/disagree. Give an overall summary of what all the sources say, but succinctly rate each source with whether they agree/disagree with the claim in one line. Also include a tally of agree/disagree/in between. If they don't reference the claim, ignore the source. Use the following up-to-date information:\n Claim:" + claim + "\n Information: " + str(formatted_data),
+                "content": "You are a fact checker LLM. Verify the claim with reference to each of the sources, rating them on how much they agree/disagree WITH THE CLAIM ({claim}) - make sure NEGATIONS (like NOT or DEBUNKS, etc) are taken into account. Give an overall summary of what all the sources say, but succinctly rate each source with whether they agree/disagree with the claim ({claim}) in one line. Also include a tally of agree/disagree/in between. If they don't reference the claim, ignore the source. Use the following up-to-date information:\n Claim:" + claim + "\n Information: " + str(formatted_data),
             },
             {
                 "role": "system",
-                "content": "The output structure should be in the following format ONLY: {summary: summary of how much the search results overall agree/disagree with the claim and why, tally: {agree: {count: number of sources that agree, sources: [{source_name: name of source, link: link to the source, summary: 1-line summary of why source agrees}, ...]}, disagree: {count: number of sources that disagree, sources: [{source_name: name of source, link: link to the source, summary: 1-line summary of why source disagrees}, ...]}, neutral: {count: number of sources that are neutral, sources: [{source_name: name of source, link: link to the source, summary: 1-line summary of why source is neutral}, ...]}}}"
+                "content": "The output structure should be in the following format ONLY (make sure to to include agree/disagree/neutral, with a 0 if there are no sources that fall into the category): {summary: summary of how much the search results overall agree/disagree WITH THE CLAIM and why, tally: {agree: {count: number of sources that agree, sources: [{source_name: name of source, link: link to the source, summary: 1-line summary of why source agrees to the claim}, ...]}, disagree: {count: number of sources that disagree, sources: [{source_name: name of source, link: link to the source, summary: 1-line summary of why source disagrees with the claim}, ...]}, neutral: {count: number of sources that are neutral, sources: [{source_name: name of source, link: link to the source, summary: 1-line summary of why source is neutral with the claim}, ...]}}}"
             }
         ]
     )
     
     return chat_response.choices[0].message.content
 
-    # l = len(chat_response.choices[0].message.content)
-    # print(chat_response.choices[0].message.content[8:l-3])
+def fact_check_fn_img(image_path, num_articles):
 
-    # return chat_response.choices[0].message.content[8:l-3]
+    def encode_image_base64(image_path):
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode("utf-8")
+
+    claim = client.chat.complete(
+        model = model,
+        messages = [
+            {
+                "role": "system",
+                "content": "You are a fact checking system helper. Your job is to find the main claim made by the user in the picture below. Take the overall claim, being specific yet succinct. "
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": f"data:image/jpeg;base64,{encode_image_base64(image_path)}"
+                    },
+                    {
+                        "type": "text",
+                        "text": "Above is a picture. Synthesize the main claim in this picture. ONLY return the claim as a single sentence string."
+                    }
+                ]
+            },
+        ]
+    )
+
+    print(claim)
+    return fact_check_fn(claim, num_articles)
+
+def fact_check_fn_video(video_path, num_articles):
+    transcription = get_transcription(video_path)
+
+    claim = client.chat.complete(
+        model = model,
+        messages = [
+            {
+                "role": "system",
+                "content": "You are a fact checking system helper. Your job is to find the main claim made by the user in the audio transcription below. Take the overall claim of the transcript, being specific yet succinct. "
+            },
+            {
+                "role": "user",
+                "content": f"Audio transcription: {transcription}. Synthesize the main claim in this transcription. ONLY return the claim as a single sentence string."
+            },
+        ]
+    )
+
+    print(claim)
+    return fact_check_fn(claim, num_articles)
